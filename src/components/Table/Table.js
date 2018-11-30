2e09ea3b6e13d6529d7905ee1dc529a35ac6ff41
Michael Melvin <michael.melvin@tribridge.com>
2018-11-29 19:44:38 -0500

    create filterable table for tour dates

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,29 +1,46 @@
 import React from 'react';
 
+import FilterableTable from 'react-filterable-table';
+
 const Table = () => {
   const data = [
-    { name: "Steve", age: 27, job: "Sandwich Eater" },
-    { name: "Gary", age: 35, job: "Falafeler" },
-    { name: "Greg", age: 24, job: "Jelly Bean Juggler" },
-    { name: "Jeb", age: 39, job: "Burrito Racer" },
-    { name: "Jeff", age: 48, job: "Hot Dog Wrangler" }
+    { date: "1/1/2019", location: 'Las Vegas, NA', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "1/15/2019", location: 'San Francisco, CA', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "2/14/2019", location: 'Atlanta, GA', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "3/3/2019", location: 'Tampa, FL', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "4/5/2019", location: 'New York, NY', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "5/5/2018", location: 'St. Louis, MS', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "5/25/2019", location: 'Sacramento, CA', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "6/11/2019", location: 'Cleveland, OH', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "6/20/2019", location: 'Nashville, TN', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "7/5/2019", location: 'Dallas, TX', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "7/15/2018", location: 'Houston, TX', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "8/1/2019", location: 'Denver, CO', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "8/28/2019", location: 'Boulder, CO', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "9/7/2019", location: 'Jacksonville, FL', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> },
+    { date: "9/27/2019", location: 'New York, NY', time: "7 PM", price: <a href='http://ticketmaster.com'>$25</a> }
   ];
 
 // Fields to show in the table, and what object properties in the data they bind to
   const fields = [
-    { name: 'name', displayName: "Name", inputFilterable: true, sortable: true },
-    { name: 'age', displayName: "Age", inputFilterable: true, exactFilterable: true, sortable: true },
-    { name: 'job', displayName: "Occupation", inputFilterable: true, exactFilterable: true, sortable: true }
+    { name: 'date', displayName: "Date", inputFilterable: true, sortable: false },
+    { name: 'location', displayName: "Location", inputFilterable: true, exactFilterable: true, sortable: true },
+    { name: 'time', displayName: "Time", inputFilterable: true, exactFilterable: true, sortable: true },
+    { name: 'price', displayName: "Price", inputFilterable: false, exactFilterable: false, sortable: false },
   ];
   return (
-    <FilterableTable
-      namespace="People"
-      initialSort="name"
-      data={data}
-      fields={fields}
-      noRecordsMessage="There are no people to display"
-      noFilteredRecordsMessage="No people match your filters!"
-    />
+    <div className="container mt-4">
+      <FilterableTable
+        namespace="react-filter-table"
+        initialSort="name"
+        data={data}
+        fields={fields}
+        noRecordsMessage="There are no people to display"
+        noFilteredRecordsMessage="No people match your filters!"
+        pagersVisible={false}
+        tableClassName='table table-dark'
+      />
+    </div>
   );
 };
 
